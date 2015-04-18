9bb8f2365c3e755c11ed730fbd5f084b650903fe
wshager <wshager@gmail.com>
2015-04-18 17:51:29 +0200

    moved conversion out, allow slashes

--- parser.js
+++ parser.js
@@ -19,6 +19,11 @@ var operatorMap = {
 exports.primaryKeyName = 'id';
 exports.lastSeen = ['sort', 'select', 'values', 'limit'];
 exports.jsonQueryCompatible = true;
+// allow slashes, process later
+// TODO convert back to inline for performance
+var chars = (/\+\*\$\-:\w%\._\//).source;
+exports.normalizeRegExp = new RegExp("(\(["+chars+",]+\)|["+chars+"]*|)([<>!]?=(?:[\w]*=)?|>|<)(\(["+chars+",]+\)|["+chars+"]*|)","g");
+exports.leftoverRegExp = new RegExp((/(\))|([&\|,])?/).source+"([/"+chars+"/]*)"+(/(\(?)/).source,"g");
 
 function parse(/*String|Object*/query, parameters){
 	if (typeof query === "undefined" || query === null)
@@ -48,12 +53,12 @@ function parse(/*String|Object*/query, parameters){
 	}
 	if(query.indexOf("/") > -1){ // performance guard
 		// convert slash delimited text to arrays
-		query = query.replace(/[\+\*\$\-:\w%\._]*\/[\+\*\$\-:\w%\._\/]*/g, function(slashed){
-			return "(" + slashed.replace(/\//g, ",") + ")";
-		});
+		//query = query.replace(/[\+\*\$\-:\w%\._]*\/[\+\*\$\-:\w%\._\/]*/g, function(slashed){
+		//	return "(" + slashed.replace(/\//g, ",") + ")";
+		//});
 	}
 	// convert FIQL to normalized call syntax form
-	query = query.replace(/(\([\+\*\$\-:\w%\._,]+\)|[\+\*\$\-:\w%\._]*|)([<>!]?=(?:[\w]*=)?|>|<)(\([\+\*\$\-:\w%\._,]+\)|[\+\*\$\-:\w%\._]*|)/g,
+	query = query.replace(exports.normalizeRegExp,
 						// <---------       property        -----------><------  operator -----><----------------   value ------------------>
 			function(t, property, operator, value){
 		if(operator.length < 3){
@@ -70,9 +75,9 @@ function parse(/*String|Object*/query, parameters){
 	if(query.charAt(0)=="?"){
 		query = query.substring(1);
 	}
-	var leftoverCharacters = query.replace(/(\))|([&\|,])?([\+\*\$\-:\w%\._]*)(\(?)/g,
-							//   <-closedParan->|<-delim-- propertyOrValue -----(> |
-		function(t, closedParan, delim, propertyOrValue, openParan){
+	var leftoverCharacters = query.replace(exports.leftoverRegExp,
+							//   <-closedParen->|<-delim-- propertyOrValue -----(> |
+		function(t, closedParen, delim, propertyOrValue, openParen){
 			if(delim){
 				if(delim === "&"){
 					setConjunction("and");
@@ -81,24 +86,25 @@ function parse(/*String|Object*/query, parameters){
 					setConjunction("or");
 				}
 			}
-			if(openParan){
+			if(openParen){
 				var newTerm = new exports.Query();
 				newTerm.name = propertyOrValue;
 				newTerm.parent = term;
 				call(newTerm);
 			}
-			else if(closedParan){
+			else if(closedParen){
 				var isArray = !term.name;
 				term = term.parent;
 				if(!term){
-					throw new URIError("Closing paranthesis without an opening paranthesis");
+					throw new URIError("Closing parenthesis without an opening parenthesis");
 				}
 				if(isArray){
 					term.args.push(term.args.pop().args);
 				}
 			}
 			else if(propertyOrValue || delim === ','){
-				term.args.push(stringToValue(propertyOrValue, parameters));
+				//term.args.push(stringToValue(propertyOrValue, parameters));
+				term.args.push(propertyOrValue);
 
 				// cache the last seen sort(), select(), values() and limit()
 				if (contains(exports.lastSeen, term.name)) {
@@ -114,7 +120,7 @@ function parse(/*String|Object*/query, parameters){
 			return "";
 		});
 	if(term.parent){
-		throw new URIError("Opening paranthesis without a closing paranthesis");
+		throw new URIError("Opening parenthesis without a closing parenthesis");
 	}
 	if(leftoverCharacters){
 		// any extra characters left over from the replace indicates invalid syntax
@@ -134,7 +140,7 @@ function parse(/*String|Object*/query, parameters){
 			term.name = operator;
 		}
 		else if(term.name !== operator){
-			throw new Error("Can not mix conjunctions within a group, use paranthesis around each set of same conjuctions (& and |)");
+			throw new Error("Can not mix conjunctions within a group, use parenthesis around each set of same conjuctions (& and |)");
 		}
 	}
 	function removeParentProperty(obj) {
@@ -168,114 +174,6 @@ exports.parseGently = function(){
 	return terms;
 }
 
-function stringToValue(string, parameters){
-	var converter = exports.converters['default'];
-	if(string.charAt(0) === "$"){
-		var param_index = parseInt(string.substring(1)) - 1;
-		return param_index >= 0 && parameters ? parameters[param_index] : undefined;
-	}
-	if(string.indexOf(":") > -1){
-		var parts = string.split(":");
-		converter = exports.converters[parts[0]];
-		if(!converter){
-			throw new URIError("Unknown converter " + parts[0]);
-		}
-		string = parts.slice(1).join(':');
-	}
-	return converter(string);
-};
-
-// FIXME this should be taken care of by typing
-// however, type inference could be necessary
-var autoConverted = exports.autoConverted = {
-	"true": true,
-	"false": false,
-	"null": null,
-	"undefined": undefined,
-	"Infinity": Infinity,
-	"-Infinity": -Infinity
-};
-
-exports.converters = {
-	auto: function(string){
-		if(autoConverted.hasOwnProperty(string)){
-			return autoConverted[string];
-		}
-		var number = +string;
-		if(isNaN(number) || number.toString() !== string){
-          /*var isoDate = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,3}))?Z$/.exec(x);
-          if (isoDate) {
-            date = new Date(Date.UTC(+isoDate[1], +isoDate[2] - 1, +isoDate[3], +isoDate[4], +isoDate[5], +isoDate[6], +isoDate[7] || 0));
-          }*/
-			string = decodeURIComponent(string);
-			if(exports.jsonQueryCompatible){
-				if(string.charAt(0) == "'" && string.charAt(string.length-1) == "'"){
-					return JSON.parse('"' + string.substring(1,string.length-1) + '"');
-				}
-			}
-			return string;
-		}
-		return number;
-	},
-	number: function(x){
-		var number = +x;
-		if(isNaN(number)){
-			throw new URIError("Invalid number " + number);
-		}
-		return number;
-	},
-	epoch: function(x){
-		var date = new Date(+x);
-		if (isNaN(date.getTime())) {
-			throw new URIError("Invalid date " + x);
-		}
-		return date;
-	},
-	isodate: function(x){
-		// four-digit year
-		var date = '0000'.substr(0,4-x.length)+x;
-		// pattern for partial dates
-		date += '0000-01-01T00:00:00Z'.substring(date.length);
-		return exports.converters.date(date);
-	},
-	date: function(x){
-		var isoDate = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,3}))?Z$/.exec(x);
-		var date;
-		if (isoDate) {
-			date = new Date(Date.UTC(+isoDate[1], +isoDate[2] - 1, +isoDate[3], +isoDate[4], +isoDate[5], +isoDate[6], +isoDate[7] || 0));
-		}else{
-			date = new Date(x);
-		}
-		if (isNaN(date.getTime())){
-			throw new URIError("Invalid date " + x);
-		}
-		return date;
-	},
-	"boolean": function(x){
-		return x === "true";
-	},
-	string: function(string){
-		return decodeURIComponent(string);
-	},
-	re: function(x){
-		return new RegExp(decodeURIComponent(x), 'i');
-	},
-	RE: function(x){
-		return new RegExp(decodeURIComponent(x));
-	},
-	glob: function(x){
-		var s = decodeURIComponent(x).replace(/([\\|\||\(|\)|\[|\{|\^|\$|\*|\+|\?|\.|\<|\>])/g, function(x){return '\\'+x;}).replace(/\\\*/g,'.*').replace(/\\\?/g,'.?');
-		if (s.substring(0,2) !== '.*') s = '^'+s; else s = s.substring(2);
-		if (s.substring(s.length-2) !== '.*') s = s+'$'; else s = s.substring(0, s.length-2);
-		return new RegExp(s, 'i');
-	}
-};
-
-// exports.converters["default"] can be changed to a different converter if you want
-// a different default converter, for example:
-// RP = require("rql/parser");
-// RP.converters["default"] = RQ.converter.string;
-exports.converters["default"] = exports.converters.auto;
 
 // this can get replaced by the chainable query if query.js is loaded
 exports.Query = function(){

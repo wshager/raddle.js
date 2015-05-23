2d233a5975d06ae463ff0b2899278d56c9ed56d1
wshager <wshager@gmail.com>
2015-05-23 17:46:25 +0200

    added core number-arithmetic funcs

---
+++ core/numeric-arithmetic-operators.js
@@ -0,0 +1,10 @@
+({define:typeof define!="undefined"?define:function(deps, factory){module.exports = factory(exports);}}).
+define(["exports"], function(exports){
+	
+	exports.add = function(arg0,arg1){
+		return arg0+arg1;
+	};
+	
+	return exports;
+	
+});

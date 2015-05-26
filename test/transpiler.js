92153ba0949062eeb9671b1e0ea7ddd8dd238f18
W.S. Hager <wshager@gmail.com>
2015-05-26 12:38:32 +0200

    added function type

--- test/transpiler.js
+++ test/transpiler.js
@@ -5,22 +5,36 @@ define(function (require) {
 		parseQuery = require('../parser').parseQuery,
 		Transpiler = require('../transpiler').Transpiler,
 		JSON = require('intern/dojo/json');
-	registerSuite({
-		name: 'rql/test/transpiling',
-		"use(core/numeric-arithmetic-operators),define(add2,(number,number),(number,number),(add(?),add(?))),add2(2,3)": function () {
-			var dfd = this.async(1000);
+	var test = {
+		name: 'raddle/test/transpiling'
+	};
+	function add(key,cb){
+		test[key] = function(){
+			var dfd = this.async(5000);
 			var transpiler = new Transpiler();
-			var ready = dfd.callback(function (error, fn) {
-		          if (error) {
-		            throw error;
-		          }
-		          assert.equal(fn(1),6,'Addition operator should add numbers together');
-			});
-			//var def = transpiler.process("define(depth,(string,number),(),(tokenize(/),count()))");
-			transpiler.process("use(core/numeric-arithmetic-operators),define(add2,(number,number),(number,number),(add(?),add(?))),add2(2,3)",function(err,fn){
+			var ready = dfd.callback(cb);
+			transpiler.process(key,function(err,fn){
 				ready(err,fn);
 			});
 			return dfd;
-		}
+		};
+	}
+	add("use(core/string-regex-functions,core/aggregate-functions),define(depth,(string,number),(),(tokenize(/),count())),depth()",function(error,fn){
+		if(error) throw error;
+		assert.equal(fn("/a/b/c"),4);
 	});
+	add("use(core/numeric-arithmetic-operators),define(add2,(number,number),(number,number),(add(?),add(?))),add2(2,3)",function (error, fn) {
+		if (error) {
+			throw error;
+		}
+		assert.equal(fn(1),6,'Addition operator should add numbers together');
+		console.log( ' ended');
+	});
+	add("use(core/numeric-arithmetic-operators,core/higher-order-functions),define(sum,(any*,any*),(),fold-left(add,0)),sum()",function (error, fn) {
+		if (error) {
+			throw error;
+		}
+		assert.equal(fn([1,2,3]),6);
+	});
+	registerSuite(test);
 });

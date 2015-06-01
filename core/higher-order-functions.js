dfb2b539b59990d0b914cc306a911819bbf1f307
W.S. Hager <wshager@gmail.com>
2015-06-01 11:57:01 +0200

    added more functions and appender arg

--- core/higher-order-functions.js
+++ core/higher-order-functions.js
@@ -10,87 +10,146 @@ define(["exports"], function(exports){
 		return accumulator;
 	}
 	
-	function unfold(init, fn, cond, r) {
-		var t;
-		if(!r) r = [];
+	function unfold(init, unspool, cond, r) {
+		r = r || [];
 		if(cond(init)) {
 			return r;
 		}
-		t = fn(init);
 		r.push(init);
-		return unfold(t, fn, cond, r);
+		return unfold(unspool(init), unspool, cond, r);
 	}
 	
-	function groupBy(init, fn, r) {
-		var t = init.length ? [init.slice(1),init[0],fn(init[0])] : false;
+	function groupBy(init, index, r) {
 		if(!r) r = {};
-		if(t===false){
+		if(init.length===0){
 			return r;
-		} else {
-			var l = r[t[2]];
-			x = l ? l : [];
-			x.push(t[1]);
-			r[t[2]] = x;
-			return groupBy(t[0], fn, r);
 		}
+		var i = index(init[0]);
+		// TODO in unfold handler, index should be checked:
+		// if no index, value is empty array, otherwise add it
+		// rewrite function below to retrieve value in single call (handler)
+		/*
+		function listifyEntry(map,key){
+			if(map[key]){
+				return map[key];
+			} else {
+				return array();
+			}
+		}
+		function unfoldTriple(init, fn, r) {
+			vat t = fn(init);
+			if(t===false){
+				return r;
+			} else {
+				x = listifyEntry(r,t[2]); // t[1] = head, t[2] = index
+				x.push(t[1]);
+				r[t[2]] = x;
+			}
+		}
+		 */
+		var v = r[i] ? r[i] : [];
+		v.push(init[0]);
+		r[i] = v;
+		// unspool = tail()
+		return groupBy(init.slice(1), index, r);
 	}
 	
-	function unfold2(init, fn, cond, next, r) {
-		var t,n;
-		if(!r) r = {};
+	function unfold2(init, unspool, cond, handler, r) {
+		// unspool prepares the next seed value
+		// cond is tested for a stop condition
+		// handler transforms the value to add
+		r = r || [];
 		if(cond(init)) {
 			return r;
 		}
-		t = fn(init);
-		n = next(init);
-		r.push(n);
-		return unfold(t, fn, cond, r);
+		r.push(handler(init));
+		return unfold2(unspool(init), unspool, cond, handler, r);
 	}
-
+	
 	/*
-	TODO implement unfold#3 with [init, fn, cond, handler]
-	seed = ["am","a","bg"];
-	var len = function(a){
-	  return a.length;
+	function head(x){
+	 return x[0];
 	}
-	groupBy(seed,len);
-	
-	groupby alternative:
-	- first triple, which in unfold example would be id()/self() and in groupBy tail()  (represents the next seed)
-	- second triple, which in groupBy would be head() (represents the actual value to add)
-	- third triple, which in groupBy would be (head(),length()) (represents the next index)
-	
-	function listifyEntry(map,key){
-		if(map[key]){
-			return map[key];
-		} else {
-			return array();
-		}
+	function tail(x){
+	 return x.slice(1);
+	}
+	function empty(x) {
+	 return x.length===0;
+	}
+	function head_strlen(x){
+	 return (head(x)+"").length;
+	}
+	function cons(x,y) {
+	  x = x || [];
+	  x.push(y);
+	  return x;
+	}
+	function map_transform(x,i,fn) {
+	  // value is curried into fn
+	  x[i] = fn(x[i]);
+	  return x;
+	}
+	function map_transform_cons(x,y,z) {
+	  return map_transform(x,y,function(a) { return cons(a,z); } );
 	}
 	
-	function unfoldTriple(init, fn, r) {
-		vat t = fn(init);
-		if(t===false){
+	unfold4(["sdf","d","we","eq","w"],tail,empty,head,head_strlen,map_transform_cons,{});
+	- unspool (next seed):
+		- example 1: (add(1))
+		- groupBy: tail()
+	- cond (stop condition):
+		- example 1: (gt(1000))
+		- groupBy: (eq(length(),0))
+	- handler (value to add): 
+		- example 1: (self())
+		- groupBy: (head())
+	- index:
+		- example 1: (null())
+		- groupBy: (head(),length())
+	- appender:
+		- example 1: (cons())
+		- groupBy: map-insert-cons#2 =>
+		map-transform(index,cons) => 
+		map transforms take index and value, because each step separately would require a stack
+		the current value for the index is transformed by a function
+		where the current value is the input:
+		map-transform(index,(add(2)))
+		however, the index is now unavailable as well, 
+		making it impossible to provide without stack shuffling or pairing
+		appender should execute against result and init, but it could be defined:
+		map-transform(?,cons(?))
+	*/
+	
+	function unfold3(init, unspool, cond, handler, index, r) {
+		// unspool prepares the next seed value
+		// cond is tested for a stop condition
+		// handler transforms the value to add
+		// index retrieves the index of the value to add
+		r = r || [];
+		if(cond(init)) {
 			return r;
-		} else {
-			x = listifyEntry(r,t[2]);
-			x.push(t[1]);
-			r[t[2]] = x;
-			return unfoldTriple(t[0], fn, r);
 		}
+		var i = index(init);
+		r[i]=handler(init);
+		return unfold3(unspool(init), unspool, cond, handler, index, r);
 	}
-	seed = ["am","a","bg"];
-	fn = function(n){
-		if(count(n))
-			return [array_slice(n,1),n[0],strlen(n[0])];
-		else
-			return false;
-	};
-	map = unfoldTriple(seed,fn,array());
-	*/ 
-
+	
+	function unfold4(init, unspool, cond, handler, index, appender, r) {
+		// unspool prepares the next seed value
+		// cond is tested for a stop condition
+		// handler transforms the value to add
+		// index retrieves the index of the value to add
+		// appender specifies how the index is inserted into the result
+		r = r || {};
+		if(cond(init)) {
+			return r;
+		}
+		return unfold4(unspool(init), unspool, cond, handler, index, appender, appender(r,index(init),handler(init)));
+	}
+	
 	exports["fold-left"] = reduce;
 	exports["unfold"] = unfold;
+	exports["unfold4"] = unfold4;
 	
 	return exports;
 	

efeab19d2e05ba3706c2b2010af4b59cf2840631
W.S. Hager <wshager@gmail.com>
2015-06-14 23:59:11 +0200

    simplified argument mapper, added dot as input identifier

--- transpiler.js
+++ transpiler.js
@@ -3,8 +3,8 @@
 ({define:typeof define!="undefined"?define:function(deps, factory){module.exports = factory(exports, require("./parser"));}}).
 define(["exports", "./parser"], function(exports, parser){
 	
-	function short(d){
-		var n = d.aname.split(":");
+	function short(qname){
+		var n = qname.split(":");
 		return (n[0]+n[1].charAt(0).toUpperCase()+n[1].substr(1)).replace(/#|-/g,"");
 	}
 	
@@ -19,7 +19,7 @@ define(["exports", "./parser"], function(exports, parser){
 		// TODO filter and separate core definitions
 		var core = value.args;
 		var reqs = core.map(function(_){
-			return "intern/dojo/text!/raddled/"+_+".rad";
+			return "intern/dojo/text!/raddled/"+_+".rdl";
 		});
 		var self = this;
 		require(core.map(function(_){ return _ }),function(){
@@ -27,7 +27,7 @@ define(["exports", "./parser"], function(exports, parser){
 			libs.forEach(function(lib,i){
 				var nslib = {};
 				for(var k in lib){
-					nslib["core:"+k] = lib[k];
+					nslib[core[i].split("/")[0]+":"+k] = lib[k];
 				} 
 				self.lib[core[i]] = nslib;
 			});
@@ -153,7 +153,7 @@ define(["exports", "./parser"], function(exports, parser){
 	};
 	
 	Transpiler.prototype.type = function(t){
-		var ts = ["null","number","string","boolean","map","function","any"];
+		var ts = ["null","number","string","boolean","node","map","function","any"];
 		// return tuple: [type,cardinality] 
 		var rt = new Array(2);
 		var card = -1;
@@ -184,87 +184,10 @@ define(["exports", "./parser"], function(exports, parser){
 		// string->any 2->7 => allow
 		// any->string 7->2 => allow (infer+check @ runtime)
 		var d = to[1]-ti[1];
-		return (ti[0]==to[0] || to[0]==6 || ti[0]==6) && d>=0 && d<=1;
+		return (ti[0]==to[0] || to[0]==7 || ti[0]==7) && d>=0 && d<=1;
 	};
 	
-	Transpiler.prototype.wrap = function(value,i,o,a,acc){
-		acc = acc || [];
-		var v = value.shift();
-		var arity = v.args.length;
-		var parts = v.name.split(":");
-		var name = parts.pop();
-		var ns = parts.length ? parts[0] : "core";
-		var aname = ns+":"+name+"#"+arity;
-		var def = this.dict[aname];
-		if(!def) {
-			throw new Error("Definition for "+aname+" not in dictionary");
-		}
-		if(i && !this.matchTypes(i,def.sigs[0])){
-			throw new Error("Type signatures do not match: "+i+"->"+def.sigs[0]);
-		}
-		//acc.unshift("("+this.lib[def.module][aname].toString()+")(");
-		acc.unshift(short(def)+"(");
-		// TODO static arg type checks
-		if(v.args){
-			if(!def.args || v.args.length!=def.args.length){
-				throw new Error("Argument length incorrect");
-			}
-		} else if(def.args) {
-			throw new Error("No arguments supplied");
-		}
-		// accumulate def hashes per run
-		if(this.cache.indexOf(aname)==-1) this.cache.push(aname);
-		// replace ? args in order
-		var args = v.args.map(function(_,i){
-			if(_=="?") {
-				if(a.length){
-					return a.shift();
-				} else {
-					throw new Error("No arguments found to replace ?");
-				}
-			} else if(typeOf(_) == "array"){
-				// compile to function
-				var f = this.compile(_,null,a);
-				return f.toString();
-			} else if(typeOf(_) == "query"){
-				// compile and execute with args
-				// TODO execute with provided args
-				// if input is null and no args, exec with null
-				var f = this.compile(_,null,a,true);
-				console.warn(f.toString())
-				return f.toString();
-			} else {
-				var t = def.args[i];
-				var r = this.convert(_);
-				if(typeof r=="string" && r.match(/^.+#[0-9]+$/)){
-					var parts = r.split(":");
-					var name = parts.pop();
-					var ns = parts.length ? parts[0] : "core";
-					var aname = ns+":"+name;
-					var d = this.dict[aname];
-					if(this.cache.indexOf(aname)==-1) this.cache.push(aname);
-					//return this.lib[d.module][r].toString();
-					return short(d);
-				}
-				// check type here
-				if(!this.typeCheck(r,t)){
-					throw new Error("Expected type ",t," for argument value ",r);
-				}
-				return JSON.stringify(r);
-			}
-		},this);
-		acc.push((args.length ? "," : "")+args.join(",")+")");
-		if(value.length) {
-			return this.wrap(value,def.sigs[1],o,a,acc);
-		} else {
-			if(o && !this.matchTypes(o,def.sigs[1])){
-				throw new Error("Type signatures do not match: "+o+"->"+def.sigs[1]);
-			}
-			return acc;
-		}
-	};
-	
-	Transpiler.prototype.compile = function(value,parent,pa,exec){
+	Transpiler.prototype.compile = function(value,parent,pa){
 		// TODO compile literals like ?
 		var self = this;
 		var top = false;
@@ -274,12 +197,12 @@ define(["exports", "./parser"], function(exports, parser){
 			value = [value];
 		}
 		if(top) this.count = 0;
-		var name,sigs=new Array(2),args=[];
+		var name,type,args=[];
 		if(parent){
 			// called from define, so compile to a definition body
 			name = parent.name;
 			args = parent.args;
-			sigs = parent.sigs;
+			type = parent.type;
 		} else {
 			name = "anon"+this.count;
 			this.count++;
@@ -287,29 +210,101 @@ define(["exports", "./parser"], function(exports, parser){
 		// if there are unknown args, take them from the definition
 		var a = [];
 		var arity = args.length;
-		for(var i=1;i<=arity;i++){
+		for(var i=1;i<arity;i++){
 			a.push("arg"+i);
 		}
 		var fa = a.slice(0);
 		fa.unshift("arg0");
-		var fargs = fa.join(",");
 		// compose the functions in the array
-		var f = this.wrap(value,sigs[0],sigs[1],parent ? a : pa);
-		// put default input arg in a
-		a.unshift("arg0");
-		var index = f.length/2;
-		f.splice(index,0,a.join(","));
+		var f = value.map(function(v){
+			var acc = [];
+			//var v = value.shift();
+			var arity = v.args.length;
+			var name = v.name;
+			var qname = (name.indexOf(":")>-1 ? name : "core:"+name)+"#"+arity;
+			/*var def = this.dict[qname];
+			if(!def) {
+				throw new Error("Definition for "+qname+" not in dictionary");
+			}*/
+			/*if(i && !this.matchTypes(i,def.sigs[0])){
+				throw new Error("Type signatures do not match: "+i+"->"+def.sigs[0]);
+			}*/
+			acc.push(short(qname));
+			// TODO static arg type checks
+			/*if(v.args){
+				if(!def.args || v.args.length!=def.args.length){
+					throw new Error("Argument length incorrect");
+				}
+			} else if(def.args) {
+				throw new Error("No arguments supplied");
+			}*/
+			// accumulate def hashes per run
+			if(this.cache.indexOf(qname)==-1) this.cache.push(qname);
+			// replace ? args in order
+			args = v.args.map(function(_,i){
+				var type = typeOf(_);
+				if(_==".") {
+					return ".";
+				} else if(_=="?") {
+					if(parent && a.length){
+						return a.shift();
+					} else if(pa.length){
+						return pa.shift();
+					} else {
+						throw new Error("No arguments found to replace ?");
+					}
+				} else if(type == "array" || type == "query"){
+					// compile to function
+					return this.compile(_,null,a).toString();
+				} else {
+					//var t = def.args[i];
+					var r = this.convert(_);
+					if(typeof r=="string" && r.match(/^.+#[0-9]+$/)){
+						var qname = (r.indexOf(":")>-1 ? r : "core:"+r);
+						//var d = this.dict[qname];
+						if(this.cache.indexOf(qname)==-1) this.cache.push(qname);
+						//return this.lib[d.module][r].toString();
+						return short(qname);
+					}
+					// check type here
+					//if(!this.typeCheck(r,t)){
+					//	throw new Error("Expected type ",t," for argument value ",r);
+					//}
+					return JSON.stringify(r);
+				}
+			},this);
+			acc.push(args);
+			return acc;
+		},this);
+		var exec = false;
+		var fn = f.reduce(function(pre,cur,i){
+		  var f = cur[0]+"(";
+		  var args = cur[1];
+		  if(args[0]=="."){
+		    args[0] = pre;
+		  } else {
+			  exec = true;
+			  f = pre+","+f;
+		  }
+		  return f+args.join(",")+")";
+		},"arg0");
+		console.warn(fn)
+		var fargs = fa.join(",");
 		var fns = "\n";
 		if(top) {
 			for(var i=0;i<this.cache.length;i++){
 				var d = this.dict[this.cache[i]]; 
-				if(d && this.lib[d.module][d.aname]){
-					fns += "var "+short(d)+"="+this.lib[d.module][d.aname].toString()+";\n";
+				if(d && this.lib[d.module][d.qname]){
+					fns += "var "+short(d.qname)+"="+this.lib[d.module][d.qname].toString()+";\n";
 				}
 			}
-			console.warn(fns)
 		}
-		return new Function("return function "+name+"("+fargs+"){ "+fns+"return "+f.join("")+";}")();
+		var func = new Function("return function "+name+"("+fargs+"){ "+fns+"return "+fn+";}")();
+		if(!exec || top){
+			return func;
+		} else {
+			return func.toString()+"()";
+		}
 	};
 	
 	Transpiler.prototype.define = function(value,params){
@@ -319,7 +314,7 @@ define(["exports", "./parser"], function(exports, parser){
 		var name = parts.pop();
 		var module = "";
 		var ns = parts.length ? parts[0] : "local";
-		var sigs = [], args = [], aname;
+		var type, args = [], qname;
 		var body,def;
 		if(l==2){
 			// lookup
@@ -327,32 +322,32 @@ define(["exports", "./parser"], function(exports, parser){
 			if(!def) {
 				throw new Error("Unknown reference in definition "+name);
 			}
-			sigs = def.sigs;
+			type = def.type;
 			args = def.args;
 			body = def.body;
 			module = def.module;
-			aname = ns+":"+name+"#"+args.length;
+			qname = ns+":"+name+"#"+args.length;
 		} else if(l==3 || l==4){
-			if(l==3 && ns=="local") {
-				ns = "core";
-			}
-			// core
-			sigs = value.args[1];
-			args = value.args[2];
-			aname = ns+":"+name+"#"+args.length;
-			if(l==4){
+			if(l==3) {
+				module = params.use;
+				if(ns=="local"){
+					ns = module.split("/")[0];
+				}
+			} else {
 				body = value.args[3];
 				module = ns;
-			} else {
-				module = params.use;
 			}
+			// core
+			args = value.args[1];
+			type = value.args[2];
+			qname = ns+":"+name+"#"+args.length;
 		}
-		if(this.dict[aname]) return this.dict[aname];
+		if(this.dict[qname]) return this.dict[qname];
 		def = {
 			name:name,
-			aname:aname,
+			qname:qname,
 			ns:ns,
-			sigs:sigs,
+			type:type,
 			args:args,
 			body:body,
 			module:module
@@ -360,9 +355,9 @@ define(["exports", "./parser"], function(exports, parser){
 		if(!this.lib[module]) this.lib[module] = {};
 		if(l==4) {
 			// compile definition
-			this.lib[module][aname] = this.compile(body,def);
+			this.lib[module][qname] = this.compile(body,def);
 		}
-		this.dict[aname] = def;
+		this.dict[qname] = def;
 		return def;
 	};
 	exports.Transpiler = Transpiler;

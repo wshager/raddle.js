9bc64f20ca9376039befc24295119f42b01abf15
W.S. Hager <wshager@gmail.com>
2018-08-18 21:31:07 +0200

    minor fixes

--- src/rich-reducers.js
+++ src/rich-reducers.js
@@ -1,6 +1,15 @@
+export function reduce(arr,fn,seed,at = 0) {
+	const l = arguments.length;
+	at = l < 3 ? 1 : at;
+	for(let item of arr) {
+		seed = fn(seed,item,at++);
+	}
+	return seed;
+}
+
 export function reduceAround(arr,fn,seed,lastSeed,nextSeed) {
 	const l = arguments.length;
-	seed = l < 3 ? arr.shift() : seed;
+	seed = l < 3 ? arr.next().value : seed;
 	let tmp = {
 		out:seed,
 		init:false,
@@ -8,13 +17,13 @@ export function reduceAround(arr,fn,seed,lastSeed,nextSeed) {
 		at: l < 3 ? 1 : 0
 	};
 	// call one 'before' to set entry
-	tmp = arr.reduce(function(tmp,next){
+	tmp = reduce(arr,function(tmp,next){
 		if(!tmp.init) {
 			tmp.entry = next;
 			tmp.init = true;
 			return tmp;
 		}
-		tmp.out = fn.call(arr,tmp.out,tmp.entry,tmp.last,next,tmp.at,arr);
+		tmp.out = fn.call(arr,tmp.out,tmp.entry,tmp.last,next,tmp.at);
 		tmp.last = tmp.entry;
 		tmp.entry = next;
 		tmp.at = tmp.at + 1;

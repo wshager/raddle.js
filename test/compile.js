0c7d8bfd1341429622cced8ab5f3eaa0b0bf2eff
W.S. Hager <wshager@gmail.com>
2018-08-18 21:32:07 +0200

    build

--- test/compile.js
+++ test/compile.js
@@ -1,9 +1,17 @@
 const rdl = require("../lib/index");
 const l3n = require("l3n");
 const n = require("frink");
+const op = require("rxjs/operators");
 
-rdl.parseString("1 + 2").pipe(l3n.toVNodeStream,$o => rdl.run($o,{
-	modules:{
-		n:n
-	}
-})).subscribe(console.log);
+//mergeMap(x => x.apply())(rdl.parseString("$<(n,\"../raddled/n.rdl\");3 - 4").pipe(l3n.toVNodeStream,x => rdl.compile(x,{n:n})))
+//rdl.parse("../raddled/n.rdl")
+// first, load all types and apply typing
+rdl.parse("../raddled/n.rdl").pipe(l3n.toVNodeStream,rdl.compile({module:{n:n}}),op.mergeMap(cx => {
+	const ret = cx.apply();
+	console.log(ret);
+	return ret;
+}))
+//rdl.parseString("3 - 4").pipe(l3n.toVNodeStream,rdl.compile({modules:{n:n}}),op.mergeMap(cx => {
+//	return cx.apply();
+//}))
+	.subscribe(x => console.log(x));

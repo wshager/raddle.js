a9bb7069b7d8210e0bc00fb1f925cdc32df29441
W.S. Hager <wshager@gmail.com>
2018-09-06 21:43:08 +0200

    always convert to iterable

--- src/rich-reducers.js
+++ src/rich-reducers.js
@@ -1,14 +1,54 @@
 export function reduce(arr,fn,seed,at = 0) {
 	const l = arguments.length;
-	at = l < 3 ? 1 : at;
-	for(let item of arr) {
-		seed = fn(seed,item,at++);
+	if(isIterable(arr)) arr = arr[Symbol.iterator]();
+	let next = {};
+	if(l < 3) {
+		next = arr.next();
+		seed = next.value;
+		at = 1;
+	}
+	while(next = arr.next(),!next.done) {
+		seed = fn(seed,next.value,at++);
 	}
 	return seed;
 }
 
+function isIterable(obj) {
+	// checks for null and undefined
+	if (obj == null) {
+		return false;
+	}
+	return typeof obj[Symbol.iterator] === "function";
+}
+
+export function reduceAhead(arr,fn,seed,nextSeed) {
+	const l = arguments.length;
+	if(isIterable(arr)) arr = arr[Symbol.iterator]();
+	seed = l < 3 ? arr.next().value : seed;
+	let tmp = {
+		out:seed,
+		init:false,
+		at: l < 3 ? 1 : 0
+	};
+	// call one 'before' to set entry
+	tmp = reduce(arr,function(tmp,next){
+		if(!tmp.init) {
+			tmp.entry = next;
+			tmp.init = true;
+			return tmp;
+		}
+		tmp.out = fn.call(arr,tmp.out,tmp.entry,next,tmp.at);
+		tmp.entry = next;
+		tmp.at = tmp.at + 1;
+		return tmp;
+	},tmp);
+	return !tmp.init ? tmp.out : fn.call(arr,tmp.out,tmp.entry,nextSeed,tmp.at);
+}
+
+
 export function reduceAround(arr,fn,seed,lastSeed,nextSeed) {
 	const l = arguments.length;
+	if(isIterable(arr)) arr = arr[Symbol.iterator]();
 	seed = l < 3 ? arr.next().value : seed;
 	let tmp = {
 		out:seed,

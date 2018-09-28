069e38ec6edfbffbd6179182c0e6fd7132d7a7ea
W.S. Hager <wshager@gmail.com>
2018-09-28 08:40:16 +0200

    updated triply to 0.1.'1

---
+++ test/reducers.js
@@ -0,0 +1,10 @@
+//const r = require("../lib/rich-reducers");
+require("array-last-item");
+const a = [1,2,3];
+
+/*const out = r.reduce(a,(a,x,n) => {
+	console.log(x,n);
+	return a+x;
+},0);*/
+
+console.log(Array.prototype.hasOwnProperty("lastItem"));
